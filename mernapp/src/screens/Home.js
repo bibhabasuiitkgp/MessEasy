1a8eb93edba6e764b633eecbeded2914d1dacfe4
Sagnibha <sagnibha123@gmail.com>
2023-08-31 23:57:31 +0530

    done almost

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Navbar1 from '../components/Navbar1'
 import Footer1 from '../components/Footer1'
+import Card from '../components/Card'
 
 export default function Home() {
   return (
     
     <div>
         <div>
-            <Navbar1/>
+            <Navbar1/>  
         </div>
         <div>
-
-       Body
+            <Card/>
         </div>
         <div><Footer1/></div>
         </div>
